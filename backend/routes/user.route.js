42ca95c3bc3a83ef832c2081639875d2ce0abecd
Pankaj Kumar <pkthapliyal101@gmail.com>
2024-02-19 22:38:41 +0530

    Deploying

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const userRoute = express.Router();
-
-const User = require("../models/user.model");
+const { User } = require("../models/user.model");
 
 userRoute.get("/", async (req, res) => {
-  res.send("Users");
+  try {
+    const users = await User.find();
+    return res.status(200).send(users);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+userRoute.post("/", async (req, res) => {
+  const { id } = req.body;
+
+  try {
+    const isUser = await User.findOne({ id: id });
+
+    if (isUser) {
+      return res.status(409).send({ message: "User is already Registered !" });
+    }
+
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    return res.status(201).send({ message: "User added", user: newUser });
+  } catch (error) {
+    return res.status(501).send({ message: "Error in adding user" });
+  }
 });
 
 module.exports = { userRoute };
