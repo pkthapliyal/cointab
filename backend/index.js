be3a574c9f74956ef7eeccc5c27a2fee0f7b98b9
Pankaj Kumar <pkthapliyal101@gmail.com>
2024-02-20 20:47:25 +0530

    Final Cors

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const { postRoute } = require("./routes/post.route");
 
 // Essential Middleware
 const corsOptions = {
-  origin: "https://cointab-8nxkejrxd-pkthapliyal.vercel.app/",
-  methods: "GET,POST, PUT",
+  origin: "https://cointab-8nxkejrxd-pkthapliyal.vercel.app",
+  methods: "GET,POST,PUT",
   credentials: true,
 };
 
